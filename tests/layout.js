5873477279be062ca40ea15d07a8c82f48436396
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-12-19 20:48:11 +0100

    Update layout.js

--- tests/layout.js
+++ tests/layout.js
@@ -1,57 +1,10 @@
+// Layout generated for JavaScript applications interacting with Streamflow's Timelock program.
 const BufferLayout = require("buffer-layout");
 const { PublicKey } = require("@solana/web3.js");
 const anchor = require("@project-serum/anchor");
 const { BN } = anchor;
 
 const LE = "le"; //little endian
-
-const StreamInstructionLayout = BufferLayout.struct([
-  BufferLayout.blob(8, "start_time"),
-  BufferLayout.blob(8, "end_time"),
-  BufferLayout.blob(8, "deposited_amount"),
-  BufferLayout.blob(8, "total_amount"),
-  BufferLayout.blob(8, "period"),
-  BufferLayout.blob(8, "cliff"),
-  BufferLayout.blob(8, "cliff_amount"),
-  BufferLayout.blob(1, "is_cancelable_by_sender"),
-  BufferLayout.blob(1, "is_cancelable_by_recipient"),
-  BufferLayout.blob(1, "is_withdrawal_public"),
-  BufferLayout.blob(1, "is_transferable"),
-  BufferLayout.blob(4, "padding"),
-]);
-
-function decode_instrcution(buf) {
-  let raw = StreamInstructionLayout.decode(buf);
-  return {
-    start_time: new BN(raw.start_time, LE),
-    end_time: new BN(raw.end_time, LE),
-    deposited_amount: new BN(raw.deposited_amount, LE),
-    total_amount: new BN(raw.total_amount, LE),
-    period: new BN(raw.period, LE),
-    cliff: new BN(raw.cliff, LE),
-    cliff_amount: new BN(raw.cliff_amount, LE),
-    is_cancelable_by_sender: Boolean(raw.is_cancelable_by_sender.readUInt8()),
-    is_cancelable_by_recipient: Boolean(
-      raw.is_cancelable_by_recipient.readUInt8()
-    ),
-    is_withdrawal_public: Boolean(raw.is_withdrawal_public.readUInt8()),
-    is_transferable: Boolean(raw.is_transferable.readUInt8()),
-  };
-}
-
-// interface StreamInstruction {
-//   start_time: BN;
-//   end_time: BN;
-//   deposited_amount: BN;
-//   total_amount: BN;
-//   period: BN;
-//   cliff: BN;
-//   cliff_amount: BN;
-//   is_cancelable_by_sender: boolean;
-//   is_cancelable_by_recipient: boolean;
-//   is_withdrawal_public: boolean;
-//   is_transferable: boolean;
-// }
 
 const TokenStreamDataLayout = BufferLayout.struct([
   BufferLayout.blob(8, "magic"),
@@ -80,7 +33,7 @@ const TokenStreamDataLayout = BufferLayout.struct([
   BufferLayout.blob(4, "padding"),
 ]);
 
-function decode(buf) {
+function decode_token_stream_data(buf) {
   let raw = TokenStreamDataLayout.decode(buf);
   return {
     magic: new BN(raw.magic, LE),
@@ -111,7 +64,6 @@ function decode(buf) {
   };
 }
 
-//
 // interface TokenStreamData {
 //   magic: BN;
 //   created_at: BN;
@@ -136,6 +88,7 @@ function decode(buf) {
 //   is_cancelable_by_recipient: boolean;
 //   is_withdrawal_public: boolean;
 //   is_transferable: boolean;
+//
 // }
 
-exports.decode = decode;
+exports.decode = decode_token_stream_data;
