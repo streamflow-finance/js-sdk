7c5b0b23ccf782b07a6f77beedac2962fc53298b
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-12-29 18:21:10 +0100

    layout.js: Updated layout manually

--- tests/layout.js
+++ tests/layout.js
@@ -10,42 +10,6 @@ var buffer_layout_1 = __importDefault(require("buffer-layout"));
 var web3_js_1 = require("@solana/web3.js");
 var anchor_1 = require("@project-serum/anchor");
 var LE = "le"; //little endian
-var StreamInstructionLayout = buffer_layout_1.default.struct([
-  buffer_layout_1.default.blob(8, "start_time"),
-  buffer_layout_1.default.blob(8, "end_time"),
-  buffer_layout_1.default.blob(8, "deposited_amount"),
-  buffer_layout_1.default.blob(8, "total_amount"),
-  buffer_layout_1.default.blob(8, "period"),
-  buffer_layout_1.default.blob(8, "cliff"),
-  buffer_layout_1.default.blob(8, "cliff_amount"),
-  buffer_layout_1.default.blob(1, "cancelable_by_sender"),
-  buffer_layout_1.default.blob(1, "cancelable_by_recipient"),
-  buffer_layout_1.default.blob(1, "withdrawal_public"),
-  buffer_layout_1.default.blob(1, "transferable by sender"),
-  buffer_layout_1.default.blob(1, "transferable by recipient"),
-  buffer_layout_1.default.blob(8, "release_rate"),
-  buffer_layout_1.default.utf8(60, "stream_name"), //  NUL-terminated C string
-]);
-
-function decode_stream_instruction(buf) {
-  var raw = StreamInstructionLayout.decode(buf);
-  return {
-    start_time: new anchor_1.BN(raw.start_time, LE),
-    end_time: new anchor_1.BN(raw.end_time, LE),
-    deposited_amount: new anchor_1.BN(raw.deposited_amount, LE),
-    total_amount: new anchor_1.BN(raw.total_amount, LE),
-    period: new anchor_1.BN(raw.period, LE),
-    cliff: new anchor_1.BN(raw.cliff, LE),
-    cliff_amount: new anchor_1.BN(raw.cliff_amount, LE),
-    cancelable_by_sender: Boolean(raw.cancelable_by_sender).valueOf(),
-    cancelable_by_recipient: Boolean(raw.cancelable_by_recipient).valueOf(),
-    withdrawal_public: Boolean(raw.withdrawal_public).valueOf(),
-    transferable_by_sender: Boolean(raw.transferable_by_sender).valueOf(),
-    transferable_by_recipient: Boolean(raw.transferable_by_recipient).valueOf(),
-    release_rate: new anchor_1.BN(raw.release_rate, LE),
-    stream_name: String(raw.stream_name),
-  };
-}
 
 var TokenStreamDataLayout = buffer_layout_1.default.struct([
   buffer_layout_1.default.blob(8, "magic"),
@@ -61,19 +25,18 @@ var TokenStreamDataLayout = buffer_layout_1.default.struct([
   buffer_layout_1.default.blob(32, "mint"),
   buffer_layout_1.default.blob(32, "escrow_tokens"),
   buffer_layout_1.default.blob(8, "start_time"),
-  buffer_layout_1.default.blob(8, "end_time"),
-  buffer_layout_1.default.blob(8, "deposited_amount"),
-  buffer_layout_1.default.blob(8, "total_amount"),
+  buffer_layout_1.default.blob(8, "net_deposited_amount"),
   buffer_layout_1.default.blob(8, "period"),
+  buffer_layout_1.default.blob(8, "amount_per_period"),
   buffer_layout_1.default.blob(8, "cliff"),
   buffer_layout_1.default.blob(8, "cliff_amount"),
   buffer_layout_1.default.blob(1, "cancelable_by_sender"),
   buffer_layout_1.default.blob(1, "cancelable_by_recipient"),
   buffer_layout_1.default.blob(1, "withdrawal_public"),
-  buffer_layout_1.default.blob(1, "transferable by sender"),
-  buffer_layout_1.default.blob(1, "transferable by recipient"),
-  buffer_layout_1.default.blob(8, "release_rate"),
-  buffer_layout_1.default.utf8(200, "stream_name"), //  it is not NUL-terminated C string
+  buffer_layout_1.default.blob(1, "transferable_by_sender"),
+  buffer_layout_1.default.blob(1, "transferable_by_recipient"),
+  buffer_layout_1.default.blob(1, "can_topup"),
+  buffer_layout_1.default.utf8(64, "stream_name"), //  NUL-terminated C string
 ]);
 
 function decode(buf) {
@@ -92,10 +55,9 @@ function decode(buf) {
     mint: new web3_js_1.PublicKey(raw.mint),
     escrow_tokens: new web3_js_1.PublicKey(raw.escrow_tokens),
     start_time: new anchor_1.BN(raw.start_time, LE),
-    end_time: new anchor_1.BN(raw.end_time, LE),
-    deposited_amount: new anchor_1.BN(raw.deposited_amount, LE),
-    total_amount: new anchor_1.BN(raw.total_amount, LE),
+    net_deposited_amount: new anchor_1.BN(raw.net_deposited_amount, LE),
     period: new anchor_1.BN(raw.period, LE),
+    amount_per_period: new anchor_1.BN(raw.amount_per_period, LE),
     cliff: new anchor_1.BN(raw.cliff, LE),
     cliff_amount: new anchor_1.BN(raw.cliff_amount, LE),
     cancelable_by_sender: Boolean(raw.cancelable_by_sender).valueOf(),
@@ -103,7 +65,7 @@ function decode(buf) {
     withdrawal_public: Boolean(raw.withdrawal_public).valueOf(),
     transferable_by_sender: Boolean(raw.transferable_by_sender).valueOf(),
     transferable_by_recipient: Boolean(raw.transferable_by_recipient).valueOf(),
-    release_rate: new anchor_1.BN(raw.release_rate, LE),
+    can_topup: Boolean(raw.can_topup).valueOf(),
     stream_name: String(raw.stream_name),
   };
 }
