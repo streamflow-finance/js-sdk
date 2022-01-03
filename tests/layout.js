0c202ea35d574ce06a847637a2fab3ccf8eab7fc
imprfekt <5047969+imprfekt@users.noreply.github.com>
2022-01-03 17:04:21 +0100

    Layout update

--- tests/layout.js
+++ tests/layout.js
@@ -13,6 +13,7 @@ var LE = "le"; //little endian
 
 var TokenStreamDataLayout = buffer_layout_1.default.struct([
   buffer_layout_1.default.blob(8, "magic"),
+  buffer_layout_1.default.blob(1, "version"),
   buffer_layout_1.default.blob(8, "created_at"),
   buffer_layout_1.default.blob(8, "withdrawn_amount"),
   buffer_layout_1.default.blob(8, "canceled_at"),
@@ -24,6 +25,16 @@ var TokenStreamDataLayout = buffer_layout_1.default.struct([
   buffer_layout_1.default.blob(32, "recipient_tokens"),
   buffer_layout_1.default.blob(32, "mint"),
   buffer_layout_1.default.blob(32, "escrow_tokens"),
+  buffer_layout_1.default.blob(32, "streamflow_treasury"),
+  buffer_layout_1.default.blob(32, "streamflow_treasury_tokens"),
+  buffer_layout_1.default.blob(8, "streamflow_fee_total"),
+  buffer_layout_1.default.blob(8, "streamflow_fee_withdrawn"),
+  buffer_layout_1.default.blob(4, "streamflow_fee_percent"),
+  buffer_layout_1.default.blob(32, "partner"),
+  buffer_layout_1.default.blob(32, "partner_tokens"),
+  buffer_layout_1.default.blob(8, "partner_fee_total"),
+  buffer_layout_1.default.blob(8, "partner_fee_withdrawn"),
+  buffer_layout_1.default.blob(4, "partner_fee_percent"),
   buffer_layout_1.default.blob(8, "start_time"),
   buffer_layout_1.default.blob(8, "net_deposited_amount"),
   buffer_layout_1.default.blob(8, "period"),
@@ -43,6 +54,7 @@ function decode(buf) {
   var raw = TokenStreamDataLayout.decode(buf);
   return {
     magic: new anchor_1.BN(raw.magic, LE),
+    version: new anchor_1.BN(raw.version, LE),
     created_at: new anchor_1.BN(raw.created_at, LE),
     withdrawn_amount: new anchor_1.BN(raw.withdrawn_amount, LE),
     canceled_at: new anchor_1.BN(raw.canceled_at, LE),
@@ -54,6 +66,18 @@ function decode(buf) {
     recipient_tokens: new web3_js_1.PublicKey(raw.recipient_tokens),
     mint: new web3_js_1.PublicKey(raw.mint),
     escrow_tokens: new web3_js_1.PublicKey(raw.escrow_tokens),
+    streamflow_treasury: new web3_js_1.PublicKey(raw.streamflow_treasury),
+    streamflow_treasury_tokens: new web3_js_1.PublicKey(
+      raw.streamflow_treasury_tokens
+    ),
+    streamflow_fee_total: new anchor_1.BN(raw.streamflow_fee_total, LE),
+    streamflow_fee_withdrawn: new anchor_1.BN(raw.streamflow_fee_withdrawn, LE),
+    streamflow_fee_percent: new anchor_1.BN(raw.streamflow_fee_percent, LE),
+    partner_fee_total: new anchor_1.BN(raw.partner_fee_total, LE),
+    partner_fee_withdrawn: new anchor_1.BN(raw.partner_fee_withdrawn, LE),
+    partner_fee_percent: new anchor_1.BN(raw.partner_fee_percent, LE),
+    partner: new web3_js_1.PublicKey(raw.partner),
+    partner_tokens: new web3_js_1.PublicKey(raw.partner_tokens),
     start_time: new anchor_1.BN(raw.start_time, LE),
     net_deposited_amount: new anchor_1.BN(raw.net_deposited_amount, LE),
     period: new anchor_1.BN(raw.period, LE),
