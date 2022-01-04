549d741da94427d622aae5b46e4d02d267d02dab
imprfekt <5047969+imprfekt@users.noreply.github.com>
2022-01-04 23:28:00 +0100

    npm: Proper layout (added, removed and properly rearranged fields)

--- packages/timelock/layout.ts
+++ packages/timelock/layout.ts
@@ -6,10 +6,11 @@ const LE = "le"; //little endian
 
 const TokenStreamDataLayout = BufferLayout.struct<Stream>([
   BufferLayout.blob(8, "magic"),
+  BufferLayout.blob(1, "version"),
   BufferLayout.blob(8, "created_at"),
   BufferLayout.blob(8, "withdrawn_amount"),
   BufferLayout.blob(8, "canceled_at"),
-  BufferLayout.blob(8, "closable_at"),
+  BufferLayout.blob(8, "end_time"),
   BufferLayout.blob(8, "last_withdrawn_at"),
   BufferLayout.blob(32, "sender"),
   BufferLayout.blob(32, "sender_tokens"),
@@ -17,11 +18,20 @@ const TokenStreamDataLayout = BufferLayout.struct<Stream>([
   BufferLayout.blob(32, "recipient_tokens"),
   BufferLayout.blob(32, "mint"),
   BufferLayout.blob(32, "escrow_tokens"),
+  BufferLayout.blob(32, "streamflow_treasury"),
+  BufferLayout.blob(32, "streamflow_treasury_tokens"),
+  BufferLayout.blob(8, "streamflow_fee_total"),
+  BufferLayout.blob(8, "streamflow_fee_withdrawn"),
+  BufferLayout.blob(4, "streamflow_fee_percent"),
+  BufferLayout.blob(32, "partner"),
+  BufferLayout.blob(32, "partner_tokens"),
+  BufferLayout.blob(8, "partner_fee_total"),
+  BufferLayout.blob(8, "partner_fee_withdrawn"),
+  BufferLayout.blob(4, "partner_fee_percent"),
   BufferLayout.blob(8, "start_time"),
-  BufferLayout.blob(8, "end_time"),
   BufferLayout.blob(8, "net_deposited_amount"),
-  BufferLayout.blob(8, "total_amount"),
   BufferLayout.blob(8, "period"),
+  BufferLayout.blob(8, "amount_per_period"),
   BufferLayout.blob(8, "cliff"),
   BufferLayout.blob(8, "cliff_amount"),
   BufferLayout.blob(1, "cancelable_by_sender"),
@@ -29,15 +39,15 @@ const TokenStreamDataLayout = BufferLayout.struct<Stream>([
   BufferLayout.blob(1, "automatic_withdrawal"),
   BufferLayout.blob(1, "transferable_by_sender"),
   BufferLayout.blob(1, "transferable_by_recipient"),
-  BufferLayout.blob(8, "amount_per_period"),
-  BufferLayout.utf8(200, "stream_name"), //  it is not NUL-terminated C string
   BufferLayout.blob(1, "can_topup"),
+  BufferLayout.utf8(200, "stream_name"), //  it is not NUL-terminated C string
 ]);
 
 export function decode(buf: Buffer) {
   let raw = TokenStreamDataLayout.decode(buf);
   return {
     magic: new BN(raw.magic, LE),
+    version: new BN(raw.version, LE),
     created_at: new BN(raw.created_at, LE),
     withdrawn_amount: new BN(raw.withdrawn_amount, LE),
     canceled_at: new BN(raw.canceled_at, LE),
@@ -49,11 +59,20 @@ export function decode(buf: Buffer) {
     recipient_tokens: new PublicKey(raw.recipient_tokens),
     mint: new PublicKey(raw.mint),
     escrow_tokens: new PublicKey(raw.escrow_tokens),
+    streamflow_treasury: new PublicKey(raw.streamflow_treasury),
+    streamflow_treasury_tokens: new PublicKey(raw.streamflow_treasury_tokens),
+    streamflow_fee_total: new BN(raw.streamflow_fee_total, LE),
+    streamflow_fee_withdrawn: new BN(raw.streamflow_fee_withdrawn, LE),
+    streamflow_fee_percent: new BN(raw.streamflow_fee_percent, LE),
+    partner_fee_total: new BN(raw.partner_fee_total, LE),
+    partner_fee_withdrawn: new BN(raw.partner_fee_withdrawn, LE),
+    partner_fee_percent: new BN(raw.partner_fee_percent, LE),
+    partner: new PublicKey(raw.partner),
+    partner_tokens: new PublicKey(raw.partner_tokens),
     start_time: new BN(raw.start_time, LE),
-    end_time: new BN(raw.end_time, LE),
     net_deposited_amount: new BN(raw.net_deposited_amount, LE),
-    total_amount: new BN(raw.total_amount, LE),
     period: new BN(raw.period, LE),
+    amount_per_period: new BN(raw.amount_per_period, LE),
     cliff: new BN(raw.cliff, LE),
     cliff_amount: new BN(raw.cliff_amount, LE),
     cancelable_by_sender: Boolean(raw.cancelable_by_sender).valueOf(),
@@ -61,14 +80,14 @@ export function decode(buf: Buffer) {
     automatic_withdrawal: Boolean(raw.automatic_withdrawal).valueOf(),
     transferable_by_sender: Boolean(raw.transferable_by_sender).valueOf(),
     transferable_by_recipient: Boolean(raw.transferable_by_recipient).valueOf(),
-    amount_per_period: new BN(raw.amount_per_period, LE),
-    stream_name: String(raw.stream_name),
     can_topup: Boolean(raw.can_topup).valueOf(),
+    stream_name: String(raw.stream_name),
   };
 }
 
 export interface Stream {
   magic: BN;
+  version: BN;
   created_at: BN;
   withdrawn_amount: BN;
   canceled_at: BN;
@@ -80,6 +99,16 @@ export interface Stream {
   recipient_tokens: PublicKey;
   mint: PublicKey;
   escrow_tokens: PublicKey;
+  streamflow_treasury: PublicKey;
+  streamflow_treasury_tokens: PublicKey;
+  streamflow_fee_total: BN;
+  streamflow_fee_withdrawn: BN;
+  streamflow_fee_percent: BN;
+  partner_fee_total: BN;
+  partner_fee_withdrawn: BN;
+  partner_fee_percent: BN;
+  partner: PublicKey;
+  partner_tokens: PublicKey;
   start_time: BN;
   end_time: BN;
   net_deposited_amount: BN;
