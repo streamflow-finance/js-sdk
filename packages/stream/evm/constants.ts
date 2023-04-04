d9d2aff3bdb12b6c4241f2ae973c0c22af068f68
Armen Nikoyan <armen@nikoyan.com>
2023-04-05 02:26:54 +0400

    feat(evm): add mainnet contract addresses

--- packages/stream/evm/constants.ts
+++ packages/stream/evm/constants.ts
@@ -1,21 +1,21 @@
 import { ICluster } from "../common/types";
 
 export const ETHEREUM_PROGRAM_IDS: Record<ICluster, string> = {
-  [ICluster.Mainnet]: "0x35fC2D13D97c13d6697Dc273fEDF93eBC92B7743",
+  [ICluster.Mainnet]: "0xedE0Bd8998BDDCb2F3fd1f4c1Be12F4614345Fb5",
   [ICluster.Devnet]: "0x35fC2D13D97c13d6697Dc273fEDF93eBC92B7743",
   [ICluster.Testnet]: "0x35fC2D13D97c13d6697Dc273fEDF93eBC92B7743",
   [ICluster.Local]: "0x35fC2D13D97c13d6697Dc273fEDF93eBC92B7743",
 };
 
 export const BNB_PROGRAM_IDS: Record<ICluster, string> = {
-  [ICluster.Mainnet]: "0xb09702834cba4c4BDb03CC7F0Fef48490a44BB50",
+  [ICluster.Mainnet]: "0xedE0Bd8998BDDCb2F3fd1f4c1Be12F4614345Fb5",
   [ICluster.Devnet]: "0xb09702834cba4c4BDb03CC7F0Fef48490a44BB50",
   [ICluster.Testnet]: "0xb09702834cba4c4BDb03CC7F0Fef48490a44BB50",
   [ICluster.Local]: "0xb09702834cba4c4BDb03CC7F0Fef48490a44BB50",
 };
 
 export const POLYGON_PROGRAM_IDS: Record<ICluster, string> = {
-  [ICluster.Mainnet]: "0x96C1A4347ed2C833C58B2B1ad6B616B4d3013C43",
+  [ICluster.Mainnet]: "0xedE0Bd8998BDDCb2F3fd1f4c1Be12F4614345Fb5",
   [ICluster.Devnet]: "0x96C1A4347ed2C833C58B2B1ad6B616B4d3013C43",
   [ICluster.Testnet]: "0x96C1A4347ed2C833C58B2B1ad6B616B4d3013C43",
   [ICluster.Local]: "0x96C1A4347ed2C833C58B2B1ad6B616B4d3013C43",
