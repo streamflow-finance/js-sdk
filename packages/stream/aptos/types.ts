7e4603e7b24b82af3ecec791335264034b834285
Yolley <Yolley@users.noreply.github.com>
2023-10-12 16:43:44 +0900

    STREAM-554: fix aptos metadata id deriving

--- packages/stream/aptos/types.ts
+++ packages/stream/aptos/types.ts
@@ -1,3 +1,10 @@
+import {
+  AptosAccount,
+  AptosClient,
+  Types,
+  TransactionBuilderRemoteABI,
+  MaybeHexString,
+} from "aptos";
 import { WalletContextState } from "@manahippo/aptos-wallet-adapter";
 import BN from "bn.js";
 
@@ -6,7 +13,7 @@ import { Stream, StreamType } from "../common/types";
 import { getNumberFromBN } from "../common/utils";
 
 export interface ICreateStreamAptosExt {
-  senderWallet: WalletContextState;
+  senderWallet: WalletContextState | AptosAccount;
 }
 
 export type ITransactionAptosExt = ICreateStreamAptosExt & {
@@ -196,5 +203,39 @@ export class Contract implements Stream {
 
   remaining(decimals: number): number {
     return getNumberFromBN(this.depositedAmount.sub(this.withdrawnAmount), decimals);
+  }
+}
+
+export class AptosWalletWrapper<T extends WalletContextState | AptosAccount> {
+  wallet: T;
+
+  address: MaybeHexString;
+
+  client: AptosClient;
+
+  constructor(wallet: T, client: AptosClient) {
+    this.client = client;
+    this.wallet = wallet;
+    if (wallet instanceof AptosAccount) {
+      this.address = wallet.address();
+    } else {
+      this.address = wallet.account!.address!;
+    }
+  }
+
+  public async signAndSubmitTransaction(
+    input: Types.TransactionPayload_EntryFunctionPayload
+  ): Promise<string> {
+    if (this.wallet instanceof AptosAccount) {
+      const builder = new TransactionBuilderRemoteABI(this.client, {
+        sender: this.address,
+      });
+      const rawTxn = await builder.build(input.function, input.type_arguments, input.arguments);
+      const signedTx = await this.client.signTransaction(this.wallet, rawTxn);
+      const tx = await this.client.submitSignedBCSTransaction(signedTx);
+      await this.client.waitForTransaction(tx.hash);
+      return tx.hash;
+    }
+    return (await this.wallet.signAndSubmitTransaction(input)).hash;
   }
 }
