59e93de81f8dcd26df10607a32f2047f784b3b35
Yolley <Yolley@users.noreply.github.com>
2023-11-03 18:46:27 +0900

    move lerna&jest to the root

--- packages/stream/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["spec.ts", "js", "ts"],
-  modulePathIgnorePatterns: ["dist", "test_dependencies"],
-};
