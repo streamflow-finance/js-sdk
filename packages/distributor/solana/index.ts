08d68135a3a4aa24ca0fcca3a722251e843e7918
Yolley <Yolley@users.noreply.github.com>
2024-03-06 21:36:20 +0700

    export from generated in distributor

--- packages/distributor/solana/index.ts
+++ packages/distributor/solana/index.ts
@@ -1,5 +1,11 @@
 export { default as SolanaDistributorClient } from "./client";
 
+export * from "./generated/accounts";
+
+export * from "./generated/errors";
+
+export * from "./generated/instructions";
+
 export * from "./utils";
 
 export * from "./types";
