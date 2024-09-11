8c243a714075e446a2ea5aa6c2f93efde97c6720
tatomir-streamflow <nikola.t@streamflow.finance>
2024-09-11 20:04:53 +0200

    separate bn conversion script from pre-commit

---
+++ scripts/convertBN.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TARGET_FOLDERS=$@
+
+OS=$(uname)
+
+if [ "$OS" = "Darwin" ]; then
+    SED_NO_BACKUP=( -i '' )
+else
+    SED_NO_BACKUP=( -i )
+fi
+
+for FOLDER in $TARGET_FOLDERS
+do
+echo "Replacing 'BN' with 'BigNumber' in $FOLDER and its subfolders"
+find "$FOLDER" -type f -name "*.ts[x]" -exec sed "${SED_NO_BACKUP[@]}" -e 's/BN/BigNumber/g' -e 's/bn/bignumber/g' {} +
+done
