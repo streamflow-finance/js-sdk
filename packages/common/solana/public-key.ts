56397d6f164f2a97749965eee4bb8feec63ce6e6
tatomir-streamflow <nikola.t@streamflow.finance>
2025-01-27 17:40:41 +0100

    address PR comments

--- packages/common/solana/public-key.ts
+++ packages/common/solana/public-key.ts
@@ -2,7 +2,11 @@ import { PublicKey } from "@solana/web3.js";
 
 /**
  * Converts a string or PublicKey to a PublicKey object.
+<<<<<<< HEAD
  * @param address - The input address as a string or PublicKey.
+=======
+ * @param key - The input key as a string or PublicKey.
+>>>>>>> e7ef5e6 (address PR comments)
  * @returns The PublicKey object.
  */
 export const pk = (address: string | PublicKey): PublicKey => {
