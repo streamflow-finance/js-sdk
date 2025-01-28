f7c13b78badacd970efc090f73e8e28a848d7484
tatomir-streamflow <nikola.t@streamflow.finance>
2025-01-28 23:42:31 +0100

    fix comment

--- packages/common/solana/public-key.ts
+++ packages/common/solana/public-key.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 
 /**
  * Converts a string or PublicKey to a PublicKey object.
- * @param key - The input key as a string or PublicKey.
+ * @param address - The input address as a string or PublicKey.
  * @returns The PublicKey object.
  */
 export const pk = (address: string | PublicKey): PublicKey => {
