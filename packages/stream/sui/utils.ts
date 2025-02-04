5ab4bf7b505bfa6a47fc5215bb629915632ea0a7
Yolley <Yolley@users.noreply.github.com>
2025-02-04 09:22:18 +0100

    STRM-798: add tx fetch with retry

--- packages/stream/sui/utils.ts
+++ packages/stream/sui/utils.ts
@@ -1,3 +1,6 @@
+import { GetTransactionBlockParams, SuiClient, SuiTransactionBlockResponse } from "@mysten/sui/client";
+import pRetry from "p-retry";
+
 import { SUI_ERROR_MATCH_REGEX, SUI_MODULE_ERROR_MAP } from "./constants.js";
 import { SuiErrorInfo } from "./types.js";
 
@@ -27,4 +30,23 @@ export function extractSuiErrorInfo(errorText: string): SuiErrorInfo {
   }
 
   return error;
+}
+
+/**
+ * Fetches transaction block with a retry:
+ * retries `Could not find the referenced transaction` error as it could be that some validators still don't have the tx;
+ *
+ * @param client sui client to use
+ * @param input transaction fetch payload,
+ */
+export function getTransactionBlock(
+  client: SuiClient,
+  input: GetTransactionBlockParams,
+): Promise<SuiTransactionBlockResponse> {
+  return pRetry(() => client.getTransactionBlock(input), {
+    retries: 5,
+    shouldRetry: (error) => error.message.includes("Could not find the referenced transaction"),
+    minTimeout: 500,
+    maxTimeout: 1000,
+  });
 }
